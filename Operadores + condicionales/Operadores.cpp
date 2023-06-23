f49b9ee1c5ef05c92f160e55a7c2026de00479b3
DianaCelesteHerrera <dayanceles@gmail.com>
2023-06-22 20:32:53 -0600

    :heavy_division_sign: Operadores/Condicionales

--- Operadores + condicionales/Operadores.cpp
+++ Operadores + condicionales/Operadores.cpp
@@ -6,19 +6,33 @@ int main(){
 	//variables asignacion 
 	int a =5 , b=10, c=15 , d=20 ;
 	bool respuesta;
+	int edad;
 	
 	//operadores logicos 
 	respuesta = (b>a);
 	
 	//bucles if 
 	if(respuesta = 1){
-		cout<<"su respuesta es cierta";
+		cout<<"su respuesta es cierta \n";
 	}else{
-		cout<<"no es verdadero";
+		cout<<"no es verdadero \n";
 	}
 	
 	
+	/*
+	Ejercicio: quieres entrar a un discoteca pero se requiere verificar tu edad
+	Para resolver el problema vamos a usar condicionales\n 
+	*/
+	cout<<"\n---------------------------------------------------------------\n";
+	cout<<"EJERCICIO: \n";
+	cout<<"Ingrese la edad que tienes: ";
+	cin>>edad;
 	
-	return 0;
-	
+	if(edad<18){
+		cout<<"\nNo puedes entrar a la discoteca\n";
+	}else{
+		cout<<"\nSi puedes entrar a la discoteca\n";
+	}
+
+return 0;
 }
